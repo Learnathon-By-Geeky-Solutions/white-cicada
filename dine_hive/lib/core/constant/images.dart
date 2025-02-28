6e3edde773d50ae66939057c766a9f2826196399
muntasir ashif <muntasirashifee@gmail.com>
2025-02-28 20:40:02 +0600

    home screen appbar edit #9

--- dine_hive/lib/core/constant/images.dart
+++ dine_hive/lib/core/constant/images.dart
@@ -7,6 +7,6 @@ class AppImages{
   static const String forgetPassImage = 'asset/images/forget_pass/forget-pass-image.png';
 
   //Home Screen
-  static const String appBarBgPattern = 'asset/images/sign_in_screen/google_logo.png';
+  static const String appBarBgPattern = 'asset/images/home_screen/appbar_pattern.png';
 
 }
